8e1af49f53799abaee2713c635b91c3708bb3184
Marek <marekz@gmail.com>
2014-02-04 11:30:07 -0500

    Init

---
+++ README.md
@@ -0,0 +1,61 @@
+# build-element plugin
+
+`build-element` builds elements fast.
+
+## Usage
+
+One function: `$.build`.
+
+Give it a tag in the style of a CSS selector.
+
+`$.build('h1#title.heading')` &rarr; `<h1 id="title" class="heading"></h1>`
+
+Pass in attributes as an extra argument.
+
+`$.build('a.link', {href: '/welcome'})` &rarr; `<a class="link" href="/welcome"></a>`
+`$.build('h1#title')` &rarr; `<h1 id="title"></h1>`
+
+`$.build('h1#title.heading')` &rarr; `<h1 id="title" class="heading"></h1>`
+
+Pass in child elements or plain text to insert into the element.
+
+`$.build('h1.heading`, 'Welcome') &rarr; `<h1 class="heading">Welcome</h1>`
+`$.build('h1.heading{Welcome}')` &rarr; `<h1 class="heading">Welcome</h1>`
+
+## Tag string syntax
+
+- `p` Plain tag
+- `p#intro` Tag with id
+- `p.highlight` Tag with class
+- `p#intro.highlight.outline` Multiple classes
+- `h1{Introduction to Calculus}` Content text in brackets.
+
+## Nesting
+
+Pass an element as an extra argument to nest it.
+
+```javascript
+var $$ = $.build; // Optionally use an alias for easy access.
+
+$$('p', $$('b{Welcome}'));
+```
+&rarr;
+```html
+&lt;p&gt;&lt;b&gt;Welcome&lt;/b&gt;&lt;/p&gt;
+```
+
+Pass an array of elements to nest them.
+
+```javascript
+$$('ul.list', [
+	$$('li.item{First Item}'),
+	$$('li.item{Second item}')
+]);
+```
+&rarr;
+```html
+&lt;ul class=&quot;list&quot;&gt;
+      &lt;li class=&quot;item&quot;&gt;First Item&lt;/li&gt;
+      &lt;li class=&quot;item&quot;&gt;Second Item&lt;/li&gt;
+     &lt;/ul&gt;
+```
---
+++ build-element.js
@@ -0,0 +1,62 @@
+/*!
+ * jQuery-build-element v0.1.0
+ * github.com/marekweb/jquery-build-element
+ * (c) 2014 Marek Zaluski www.marekweb.com
+ * Released under the MIT license
+ */
+
+(function() {
+
+	function parseDescriptor(s) {
+		var items = [];
+		while (true) {
+			var char = s.charAt(0);
+			var type;
+			if (char == "#") {
+				type = "id";
+				s = s.slice(1);
+			} else if (char == ".") {
+				type = "class";
+				s = s.slice(1);
+			} else if (char == "{") {
+				var close = s.indexOf('}');
+				if (close === -1) throw "Missing closing curly bracket in: " + s;
+				items.push(["text", s.slice(1, close)]);
+				s = s.slice(close+1);
+				continue;
+			} else type = "tag";
+			var i = s.search(/[#.{]/);
+			if (i === -1) {
+				if (s.length != 0) items.push([type, s]);
+				return items;
+			}
+			items.push([type, s.slice(0, i)]);
+			s = s.slice(i);
+		}
+	}
+
+	$.build = function(desc, attrs, content) {
+		desc = desc || "";
+		if (!$.isPlainObject(attrs) && !content) {
+			content = attrs;
+			attrs = {};
+		}
+		var items = parseDescriptor(desc);
+		var classes = [];
+		var id = "";
+		var tag = "div";
+		var text;
+		for (var i = 0, len = items.length; i < len; i++) {
+			var item = items[i];
+			if (item[0] == "tag") tag = item[1];
+			if (item[0] == "id") id = item[1];
+			if (item[0] == "class") classes.push(item[1]);
+			if (item[0] == "text") content = item[1];
+		}
+		attrs = attrs || {};
+		if (classes.length > 0) attrs["class"] = classes.join(' ');
+		if (id) attrs.id = id;
+		return $('<' + tag + '>').attr(attrs).append(content);
+	}
+
+})();
